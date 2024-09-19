76a778e5b7a0e39601c3b1ee43c8a23d804607ed
vicky <nahye0729@naver.com>
2024-09-19 14:33:27 +0900

    UserDAO추가

--- src/main/java/Model/UserDTO.java
+++ src/main/java/Model/UserDTO.java
@@ -1,5 +1,14 @@
 package Model;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+@Getter
 public class UserDTO {
 	
 	   // 회원 아이디 
