39e0c85575aa92917556f872f8818f4f52b0ff5c
vicky <nahye0729@naver.com>
2024-09-19 14:23:42 +0900

    UserDTO생성

---
+++ src/main/java/Model/UserDTO.java
@@ -0,0 +1,21 @@
+package Model;
+
+public class UserDTO {
+	
+	   // 회원 아이디 
+    private String userId;
+
+    // 회원 비밀번호 
+    private String userPw;
+
+    // 회원 이름 
+    private String userName;
+
+    // 회원 이메일 
+    private String userEmail;
+
+
+    // 회원 프로필사진 
+    private String userProfileImg;
+
+}
