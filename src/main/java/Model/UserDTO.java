b1110fb4d26207f84bcbda79bd3ce9feb05d208f
Vicky0729 <nahye0729@gmail.com>
2024-09-19 22:08:28 +0900

    join.userDTO

--- src/main/java/Model/UserDTO.java
+++ src/main/java/Model/UserDTO.java
@@ -11,7 +11,9 @@ import lombok.NoArgsConstructor;
 @Getter
 public class UserDTO {
 	
-	   // 회원 아이디 
+
+
+	// 회원 아이디 
     private String userId;
 
     // 회원 비밀번호 
@@ -23,6 +25,14 @@ public class UserDTO {
     // 회원 이메일 
     private String userEmail;
 
+    public UserDTO(String userId, String userPw, String userName, String userEmail, String userProfileImg) {
+    	super();
+    	this.userId = userId;
+    	this.userPw = userPw;
+    	this.userName = userName;
+    	this.userEmail = userEmail;
+    	this.userProfileImg = userProfileImg;
+    }
 
     // 회원 프로필사진 
     private String userProfileImg;
